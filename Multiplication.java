78ad528e1e4f68d526c06a39c885caf264308212
Hinafemin <118240617+Hinafemin@users.noreply.github.com>
2023-08-21 11:05:22 +0530

    Delete Multiplication.java

--- Multiplication.java
+++
@@ -1,9 +0,0 @@
-package arithmetic;
-interface Arithmetic {
-    double calculate(double num1, double num2);
-    }
-public class Multiplication implements Arithmetic {
-    public double calculate(double num1, double num2) {
-        return num1 * num2;
-    }
-}
